993a150f3987edbde76bfc5bc6a68996d45246f7
danricardo88 <daniel.ricardo88@outlook.com>
2022-08-07 19:27:50 -0300

    Req 5 - ok / Deus ajuda quem cedo madruga

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,11 +1,50 @@
 import React, { Component } from 'react';
 import Header from '../components/Header';
+// import Loading from './Loading';
 
 export default class Search extends Component {
+  state = {
+    isDisable: true,
+    // loading: false,
+    inputSearch: '',
+
+  };
+
+  handleSearch = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    }, () => {
+      const { inputSearch } = this.state;
+      const searchLength = inputSearch.length;
+      const minSearch = 2;
+      const test = searchLength < minSearch;
+      this.setState({ isDisable: test });
+    });
+  }
+
   render() {
+    const { inputSearch, isDisable /* loading */ } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
+        <fieldset>
+          <input
+            data-testid="search-artist-input"
+            placeholder="Pesquise uma banda ou artista."
+            type="inputSearch"
+            name="inputSearch"
+            value={ inputSearch }
+            onChange={ this.handleSearch }
+          />
+          <button
+            data-testid="search-artist-button"
+            type="button"
+            name="buttonSearch"
+            disabled={ isDisable }
+          >
+            Pesquisar
+          </button>
+        </fieldset>
       </div>
     );
   }
