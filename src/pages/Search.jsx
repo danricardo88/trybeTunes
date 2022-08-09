511a0adeafa25d685cf1cac6aceeaf8281086d1c
danricardo88 <daniel.ricardo88@outlook.com>
2022-08-09 04:33:29 -0300

    Ta complicado

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-// import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
-// import Loading from './Loading';
+import Loading from '../components/Loading';
 import searchAlbumAPI from '../services/searchAlbumsAPI';
 
 export default class Search extends Component {
@@ -21,6 +21,22 @@ export default class Search extends Component {
   //     album: apiSearch,
   //   });
   // }
+  handleSearch = async () => {
+    this.setState({ loading: true });
+    const { inputSearch } = this.state;
+    const albuns = await searchAlbumAPI(inputSearch);
+    this.setState({
+      album: albuns,
+      loading: false,
+      artist: inputSearch,
+      inputSearch: '',
+    }, () => {
+      const min = 0;
+      const testAlbum = album.length;
+      const test = testAlbum === min;
+      this.setState({ isDisable: test });
+    });
+  }
 
   handleInputSearch = (e) => {
     this.setState({
@@ -34,29 +50,9 @@ export default class Search extends Component {
     });
   };
 
-  handleSearch = (e) => {
-    e.preventDeFault();
-    this.setState({ loading: true }, async () => {
-      const { inputSearch } = this.state;
-      const album = await searchAlbumAPI(inputSearch);
-      this.setState({
-        album,
-        loading: false,
-
-        artist: inputSearch,
-        inputSearch: '',
-      }, () => {
-        const min = 0;
-        const testAlbum = album.length;
-        const test = testAlbum === min;
-        this.setState({ isDisable: test });
-      });
-    });
-  }
-
   render() {
-    const { inputSearch, isDisable, loading } = this.state;
-    return (
+    const { inputSearch, isDisable, loading, album, artist } = this.state;
+    return loading ? <Loading /> : (
       <div data-testid="page-search">
         <Header />
         <fieldset>
@@ -73,16 +69,55 @@ export default class Search extends Component {
             type="button"
             name="buttonSearch"
             disabled={ isDisable }
+            onClick={ this.handleSearch }
           >
             Pesquisar
           </button>
         </fieldset>
-        <div> </div>
+        {/* {
+          album.length === 0 && (<p>`Nenhum álbum foi encontrado`</p>)
+        } */}
+        {
+          album.length > 0(
+            <main>
+              <p>
+                Resultado:
+                {''}
+                {artist}
+              </p>
+              {
+                album.map(({
+                  artistName,
+                  collectionId,
+                  collectionName,
+                  // collectionPrice: 12.25,
+                  artworkUrl100,
+                }) => (
+                  <div key={ collectionId }>
+                    <h1>
+                      {''}
+                      { collectionName }
+                    </h1>
+                    <img src={ artworkUrl100 } alt={ artistName } />
+                    <h2>{ artistName }</h2>
+                    <Link
+                      to={ `/album/${collectionId}` }
+                      data-testid={ `link-to-album-${collectionId}` }
+                    >
+                      Selecione o álbum
+                    </Link>
+                  </div>
+                ))
+              }
+            </main>,
+          )
+        }
+        {
+          album.length === 0 && (<p>`Nenhum álbum foi encontrado`</p>)
+        }
       </div>
     );
   }
 }
-
-//  Search;
 
 // A rota /search deve renderizar um componente chamado Search. Este componente deve ter uma div que envolva todo seu conteúdo e ter o atributo data-testid="page-search"
