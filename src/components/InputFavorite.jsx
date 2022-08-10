f3ceea4041d87df25ab152c2276cf1bf5194da12
danricardo88 <daniel.ricardo88@outlook.com>
2022-08-10 05:24:18 -0300

    ACABOU TENHO FAMILIA

--- src/components/InputFavorite.jsx
+++ src/components/InputFavorite.jsx
@@ -1,34 +1,42 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { addSong, removeSong } from '../services/favoriteSongsAPI';
+import { addSong, getFavoriteSongs, removeSong } from '../services/favoriteSongsAPI';
 import Loading from './Loading';
 
 export default class ImputFavorite extends Component {
   state = {
-    // favoriteCheck: false,
+    favoriteCheck: false,
     // favorite: [],
     loading: false,
+  }
+
+  componentDidMount = async () => {
+    const fav = await getFavoriteSongs();
+    const { trackId } = this.props;
+    this.setState({
+      favoriteCheck: fav.some((music) => music.trackId === trackId),
+    });
   }
 
   handleFavoriteCheck = async ({ target: { checked } }, music) => {
     this.setState({ loading: true });
     if (checked) {
       await addSong(music);
+      this.setState({ favoriteCheck: true, loading: false });
     } else {
       await removeSong(music);
+      this.setState({ favoriteCheck: false, loading: false });
     }
-    console.log(music);
-    this.setState({ loading: false });
   };
 
   render() {
-    const { loading } = this.state;
+    const { loading, favoriteCheck } = this.state;
     const { music, trackId } = this.props;
     return (
       <label htmlFor="favorite">
         {loading && <Loading /> }
         <input
-          // checked={ favoriteCheck }
+          checked={ favoriteCheck }
           type="checkbox"
           name="favorite"
           onChange={ (e) => this.handleFavoriteCheck(e, music) }
